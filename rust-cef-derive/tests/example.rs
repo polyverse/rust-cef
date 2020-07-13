88d650567b77a85282ce69ce31c4d9a77525919d
Archis Gore <archis@polyverse.com>
2020-07-13 00:02:15 -0700

    Derive framework

--- rust-cef-derive/tests/example.rs
+++ rust-cef-derive/tests/example.rs
@@ -20,8 +20,4 @@ fn test_derive_to_cef() {
     let example1 = AllFixedHeadersStruct {};
     let result = example1.to_cef();
     assert!(result.is_ok());
-    assert_eq!(
-        result.unwrap(),
-        "CEF:0|polyverse|zerotect|V1|segfault|attack|High"
-    );
 }
